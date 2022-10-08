88ddcf245b8b60ba3ba69c005707adb84301b601
ssexton <steve@sextons.net>
2022-10-08 09:46:30 -0400

    package LICENSE, README.md to docs/jinn/ directory.  LICENSE, README.md at root of repo is good for gitlab, not so good for deps.sh

--- package-jinn.sh
+++ package-jinn.sh
@@ -7,10 +7,13 @@ out=$b/jinn.tar
 rm -f $out*
 if [ "$OS" == "Windows_NT" ]
 then
-  tar cf$v $out --transform=s,bin/,tools/,g bin/jinn.{exe*,pdb} bin/lua53.{exe*,dll} jinnfile.lua lua/*.lua jgen.sh README.md LICENSE
+  tar cf$v $out --transform=s,bin/,tools/,g bin/jinn.{exe*,pdb} bin/lua53.{exe*,dll} 
 else
-  tar cf$v $out --transform=s,bin/,tools/,g bin/jinn bin/lua53 jinnfile.lua lua/*.lua jgen.sh README.md LICENSE
+  tar cf$v $out --transform=s,bin/,tools/,g bin/jinn bin/lua53
 fi
+tar rf$v $out jinnfile.lua lua/*.lua jgen.sh 
+tar rf$v $out --transform=s,README.md,docs/jinn/README.md,g README.md
+tar rf$v $out --transform=s,LICENSE,docs/jinn/LICENSE,g LICENSE
 echo Compressing...
 gzip $out
 tar tf $out* | sort 
